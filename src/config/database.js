d2cf54b163c2698a55fc6aa6de4c5862e179c46d
Pedro <pedro.lucas210600@gmail.com>
2020-08-23 14:22:37 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '',
+  database: 'helper',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
