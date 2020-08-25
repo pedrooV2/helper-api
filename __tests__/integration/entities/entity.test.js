1c1f863d694b94f785468a7c65a2954ca2b92f46
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-08-25 13:42:37 -0300

    tests/duplicated email

--- __tests__/integration/entities/entity.test.js
+++ __tests__/integration/entities/entity.test.js
@@ -3,7 +3,7 @@ import app from '../../../src/app';
 
 describe('Entity', () => {
   it('should be able register entities', async () => {
-    const response = request(app).post('/entities').send({
+    const response = await request(app).post('/entities').send({
       name: 'entity name',
       email: 'mail@entity.com',
       password: '123456',
@@ -11,5 +11,21 @@ describe('Entity', () => {
 
     expect(response.body).toHaveProperty('id');
     expect(response.status).toBe(201);
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app).post('/entities').send({
+      name: 'entity name',
+      email: 'mail@entity.com',
+      password: '123456',
+    });
+
+    const response = await request(app).post('/entities').send({
+      name: 'entity name',
+      email: 'mail@entity.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(400);
   });
 });
