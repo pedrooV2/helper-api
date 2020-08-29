81ac51a36f8bd3d6e16bddd537a50853b746b2a3
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-08-29 15:50:19 -0300

    password validation

--- src/app/controllers/EntityController.js
+++ src/app/controllers/EntityController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
 import Entity from '../models/Entity';
 
 class EntityController {
   async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email } = request.body;
 
     const entityExists = await Entity.findOne({
