5f7852b8ec4a8d26bbdac62e2beadfe0b5ee6b33
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-09-06 10:50:47 -0300

    improve donators tests

--- __tests__/integration/donators/donator.test.js
+++ __tests__/integration/donators/donator.test.js
@@ -1,6 +1,8 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../../src/app';
 
+import DonatorFactory from '../../factories/donator';
 import truncate from '../../util/truncate';
 
 describe('Donator', () => {
@@ -9,44 +11,51 @@ describe('Donator', () => {
   });
 
   it('should be able to register donators', async () => {
-    const response = await request(app).post('/donators').send({
-      email: 'mail@mail.com',
-      password: '123456',
-    });
+    const donator = await DonatorFactory.attrs('Donator');
+
+    const response = await request(app).post('/donators').send(donator);
 
     expect(response.body).toHaveProperty('id');
     expect(response.status).toBe(201);
   });
 
   it('should not be able register donator with duplicated email', async () => {
-    await request(app).post('donators').send({
-      email: 'mail@mail.com',
-      password: '123456',
-    });
+    const donator = await DonatorFactory.attrs('Donator');
 
-    const response = await request(app).post('donators').send({
-      email: 'mail@mail.com',
-      password: '123456',
-    });
+    await request(app).post('donators').send(donator);
+
+    const response = await request(app).post('donators').send(donator);
 
     expect(response.status).toBe(400);
   });
 
   it('should not be able register donator with password length less than six', async () => {
-    const response = await request(app).post('/donators').send({
-      email: 'mail@mail.com',
+    const donator = await DonatorFactory.attrs('Donator', {
       password: '12345',
     });
+
+    const response = await request(app).post('/donators').send(donator);
 
     expect(response.status).toBe(400);
   });
 
   it('should not be able register donator with invalid email', async () => {
-    const response = await request(app).post('/donators').send({
+    const donator = await DonatorFactory.attrs('Donator', {
       email: 'mail',
+    });
+
+    const response = await request(app).post('/donators').send(donator);
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should encrypt donator password when new donator created', async () => {
+    const donator = await DonatorFactory.create('Donator', {
       password: '123456',
     });
 
-    expect(response.status).toBe(400);
+    const compareHash = await bcrypt.compare('123456', donator.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 });
