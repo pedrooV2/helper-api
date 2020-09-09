a95ab8aad4d1794c8dd6bed9dbeed7c28a656805
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-09-09 10:34:15 -0300

    cases migration

---
+++ src/database/migrations/20200909133102-create-cases.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cases', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      value: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      value_collected: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      opened: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('cases');
+  },
+};
