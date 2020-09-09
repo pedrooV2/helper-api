d0fbd1b937e66efab1ce7a4d39053627319f51bc
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-09-09 10:30:37 -0300

    tests/create cases

--- __tests__/integration/cases.test.js
+++ __tests__/integration/cases.test.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 import app from '../../src/app';
 
 import CaseFactory from '../factories/case';
+import EntityFactory from '../factories/entity';
 
 import truncate from '../util/truncate';
 
@@ -10,5 +11,34 @@ describe('Cases', () => {
     await truncate();
   });
 
-  it('should not be able create cases without authenticate', async () => {});
+  it('should not be able create cases without authenticate', async () => {
+    const caseAttr = await CaseFactory.attrs('Case');
+
+    const response = await request(app).post('/cases').send(caseAttr);
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should be able create cases when authenticate', async () => {
+    const entity = await EntityFactory.create('Entity', {
+      password: '123456',
+    });
+
+    const auth = await request(app).post('/entity/auth').send({
+      email: entity.email,
+      password: '123456',
+    });
+
+    const { token } = auth.body;
+
+    const caseAttr = await CaseFactory.attrs('Case');
+
+    const response = await request(app)
+      .post('/cases')
+      .send(caseAttr)
+      .set('authorization', `Bearer ${token}`);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.status).toBe(201);
+  });
 });
