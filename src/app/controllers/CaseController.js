f5dd329f13610d6337043294df2766db15f43954
Pedro <pedro.lucas210600@gmail.com>
2020-09-10 11:25:19 -0300

    create Cases

--- src/app/controllers/CaseController.js
+++ src/app/controllers/CaseController.js
@@ -1,4 +1,12 @@
+import Case from '../models/Case';
+
 class CaseController {
-  async store(request, response) {}
+  async store(request, response) {
+    request.body.entity_id = request.entity_id;
+
+    const caseModel = await Case.create(request.body);
+
+    return response.status(201).json(caseModel);
+  }
 }
 export default new CaseController();
