7cc490cbb355604b2b2b76f37f83ad7399293dc7
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-14 20:06:46 -0300

    implement cache on dashboard endpoint

--- src/app/controllers/Entity/DashboardController.js
+++ src/app/controllers/Entity/DashboardController.js
@@ -1,9 +1,16 @@
 import Case from '../../models/Case';
 import Donation from '../../models/Donation';
+import Cache from '../../../libs/Cache';
 
 class DashboardController {
   async index(request, response) {
     const { id: entity_id } = request;
+
+    const cached = await Cache.get(`entity:${entity_id}:dashboard`);
+
+    if (cached) {
+      return response.json(cached);
+    }
 
     const cases = await Case.findAll({ where: { entity_id } });
 
@@ -19,6 +26,12 @@ class DashboardController {
 
     const activeCasesCount = await Case.count({
       where: { entity_id, opened: true },
+    });
+
+    Cache.set(`entity:${entity_id}:dashboard`, {
+      activeCases: activeCasesCount,
+      donationsCount,
+      totalAmount: totalAmount / 100,
     });
 
     return response.json({
