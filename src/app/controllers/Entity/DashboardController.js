fb339907af9a77e5e68acdd53ac07ee404c87375
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-14 19:50:36 -0300

    get entity dashboard data

--- src/app/controllers/Entity/DashboardController.js
+++ src/app/controllers/Entity/DashboardController.js
@@ -1,14 +1,31 @@
 import Case from '../../models/Case';
+import Donation from '../../models/Donation';
 
 class DashboardController {
   async index(request, response) {
     const { id: entity_id } = request;
 
+    const cases = await Case.findAll({ where: { entity_id } });
+
+    const casesId = cases.map(({ id }) => id);
+
+    const donationsCount = await Donation.count({
+      where: { case_id: casesId },
+    });
+
+    const totalAmount = await Donation.sum('value', {
+      where: { case_id: casesId },
+    });
+
     const activeCasesCount = await Case.count({
       where: { entity_id, opened: true },
     });
 
-    return response.json({ activeCases: activeCasesCount });
+    return response.json({
+      activeCases: activeCasesCount,
+      donationsCount,
+      totalAmount: totalAmount / 100,
+    });
   }
 }
 
