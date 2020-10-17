130e794e5da6615a95aa0960592b76f158e1b519
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-17 13:01:50 -0300

    add cache to case details

--- src/app/controllers/CaseController.js
+++ src/app/controllers/CaseController.js
@@ -32,6 +32,12 @@ class CaseController {
     const { id } = request.params;
     const { id: entity_id } = request;
 
+    const cached = await Cache.get(`entity:${entity_id}:case:${id}`);
+
+    if (cached) {
+      return response.status(200).json(cached);
+    }
+
     const { error, data, statusCode } = await new CaseService().getCaseById({
       id,
       entity_id,
@@ -40,6 +46,8 @@ class CaseController {
     if (error) {
       return response.status(statusCode).json({ error });
     }
+
+    await Cache.set(`entity:${entity_id}:case:${id}`, { ...data });
 
     return response.status(statusCode).json({ ...data });
   }
