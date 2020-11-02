2729825b03a90c60d88b3f09a3d9dafe4b72948d
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-11-02 16:27:20 -0300

    card store

--- src/app/controllers/Donator/CardController.js
+++ src/app/controllers/Donator/CardController.js
@@ -1,6 +1,12 @@
+import Card from '../../models/Card';
+
 class CardController {
   async store(request, response) {
-    return response.json();
+    const { id: donator_id } = request;
+
+    const card = await Card.create({ donator_id, ...request.body });
+
+    return response.status(201).json(card);
   }
 }
 
